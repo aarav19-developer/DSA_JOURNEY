30edcfc3714b5a92329b339bb27f37a3bc3d936c
Aarav Harit <aaravharit418@gmail.com>
2026-04-12 16:02:39 +0530

    JOURNEY_PART

---
+++ Doubly_Linked_List.py
@@ -0,0 +1,10 @@
+# DOUBLY LINKED LIST:-
+                 # Doubly linked list is a linear data structure.
+
+# ELEMENTARY OPERATIONS:
+                # Insertion
+                # Deletion
+                # Traversing
+                # Searching
+                # Checking for empty list
+ 
