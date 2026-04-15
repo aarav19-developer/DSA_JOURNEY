68d6aafc37f73418008aff94601afdbf83b21b54
Aarav Harit <aaravharit418@gmail.com>
2026-04-16 00:36:58 +0530

    JOURNEY_PART

--- Doubly_Linked_List.py
+++ Doubly_Linked_List.py
@@ -12,11 +12,11 @@
 
 # Define a class Node to describe a node of a doubly linked list.
 
-# class Node:
-#     def __init__(self,prev=None,item= None,next=None):
-#         self.prev = prev
-#         self.item = item
-#         self.next = next
+class Node:
+    def __init__(self,prev=None,item= None,next=None):
+        self.prev = prev
+        self.item = item
+        self.next = next
 
 # Define a class DLL to impliment Doubly Linked List with __init__( )method to create and initialize start refrence variable.
 
@@ -32,7 +32,131 @@ class DLL:
 # In class DLL, define a method insert_at_start() to insert an element at the starting of the list.
 
     def insert_at_start(self,data):
+        n = Node(None,data,self.start)
+        if not self.is_empty():
+            self.start.prev = n
+        self.start = n    
+
+# In class DLL, define a method insert_at_last() to insert an element at the last of the list.
+
+    def insert_at_last(self,data):
+        temp=self.start
+        if self.start != None:
+            while temp.next != None:
+                temp = temp.next
         
+        n = Node(temp,data,None)
+        if temp == None:
+            self.start = n
+        else:
+            temp.next =n
+
+# In class DLL, define a method search() to find the node with specified element value.
+
+    def search(self,data):
+        temp = self.start
+        while temp != None:
+            if temp.item ==data:
+                return temp
+            temp = temp.next
+        return None
+
+# In class DLL, define a method insert_after() to insert a new code after a given code of the list.
+
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n = Node(temp,data,temp.next)
+            if temp.next is not None:
+                temp.next.prev = n
+            temp.next = n
+        
+
+# In class DLL, define a method to print all the elements of the list.
+
+    def print_list(self):
+        temp = self.start
+        while temp is not None:
+            print(temp.item,end = '')
+            temp = temp.next
+        
+# In class DLL, define a method delete_first() to delete first element from the list.
+
+    def delete_first(self):
+        if self.start is not None:
+            self.start = self.start.next
+            if self.start is not None:
+                self.start.prev = None
+
+# In class DLL, def delete_last() to delete first element from the list.
+
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start. next is None:
+            self.start = None
+        else:
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next
+            temp.prev.next = None
+    
+# In class DLL, def delete_item() to delete any specific element from the list.
+
+    def delete_item(self,data):
+        if self.start is None:
+            pass
+        else:
+            temp=self.start
+            while temp is not None:
+                if temp.item == data:
+                    if temp.next is not None:
+                        temp.next.prev = temp.prev
+                    if temp.prev is not None:
+                        temp.prev.next = temp.next
+                    else:
+                        self.start = temp.next
+                    break
+                temp = temp.next
+
+# In a class DLL, impliment iterator for DLL to access all the elements of the list in a sequence.
+
+    def __iter__(self):
+        return DLLIterator(self.start)
+    
+class DLLIterator:
+    def __init__(self,start):
+        self.current = start
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if not self.current:
+            raise StopIteration
+        data = self.current.item
+        self.current = self.current.next
+        return data
+    
+mylist = DLL()
+mylist.insert_at_start(22)
+mylist.insert_at_last(13)
+mylist.insert_after(mylist.search(22),1322)
+for x in mylist:
+    print(x,end=' ')
+print()
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
